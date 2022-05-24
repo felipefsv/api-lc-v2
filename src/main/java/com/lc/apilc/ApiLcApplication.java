1268150fb03edbebcb41594fcabae57722de9e8a
Felipe Viana <felipefsv@hotmail.com>
2022-05-24 10:32:29 -0300

    jwt.signature ENV

--- src/main/java/com/lc/apilc/ApiLcApplication.java
+++ src/main/java/com/lc/apilc/ApiLcApplication.java
@@ -1,31 +1,12 @@
 package com.lc.apilc;
 
-import com.lc.apilc.models.entity.Department;
-import com.lc.apilc.models.entity.User;
-import com.lc.apilc.services.DepartmentService;
-import com.lc.apilc.services.JwtService;
-import com.lc.apilc.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication()
 public class ApiLcApplication {
-
-    @Autowired
-    private DepartmentService departmentService;
-
-    @Autowired
-    private UserService userService;
-
     public static void main(String[] args) {
         SpringApplication.run(ApiLcApplication.class, args);
     }
@@ -36,5 +17,4 @@ public class ApiLcApplication {
             System.out.println("######### RUNNING ############");
         };
     }
-
 }
